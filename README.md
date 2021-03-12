48752d22792a9cd6414ccd633b5850e77de2a554
hannah26hannah <ooyoonoo24@gmail.com>
2021-03-13 03:00:26 +0900

    🔧 setup basic configuration

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Clone Twitter
 - React
+
+# Process
+- 2021.03.13 ~ #2.0
+
+# Reference
+Twitter Clone Coding from Nomad Coders
---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import AppRouter from 'components/Router'
+import {authService} from 'fBase';
+
+function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+    
+
+    return (
+        <>
+            <AppRouter isLoggedIn={isLoggedIn} />
+            <footer>&copy; ClonedTwitter {new Date().getFullYear()}</footer>
+        </>
+    ) 
+}
+
+export default App
